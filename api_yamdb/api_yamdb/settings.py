2312e9cd72b6b73a20a53a435bb33653cdd6531e
Валентин Веселов <valentin-veselov@mail.ru>
2023-03-18 12:26:26 +0300

    sixth

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ from django.conf.global_settings import DATETIME_INPUT_FORMATS
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
